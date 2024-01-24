0796ad1be9cfbc2ed02e5be85fd8ff4e3743b222
RedElephant <info@redelephant.republican>
2024-01-23 18:43:26 -0600

    Initial commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Brian Schoenholtz\Desktop\joke_assistant\venv
+set VIRTUAL_ENV=C:\Users\Brian Schoenholtz\Desktop\joke-assistant\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
